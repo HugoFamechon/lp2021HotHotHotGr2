1f10e3f6038eccc2dd4ad600dd3d3eafd70df878
NAVARRO Anthony <anthony.navarro@etu.univ-amu.fr>
2021-01-12 10:16:42 +0100

    Ajout des images

--- Vues/gabarit.php
+++ Vues/gabarit.php
@@ -4,12 +4,12 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Project HotHotHot</title>
+        <link rel="stylesheet" href="../Assets/css/bootstrap.css">
         <link rel="stylesheet" href="../Assets/css/notie.min.css">
         <link rel="stylesheet" href="../Assets/css/style.css">
         <link rel="stylesheet" href="../Assets/fonts/stylesheet.css">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
     </head>
-    <body>
+    <body class="container-fluid">
         <?php Vue::montrer('standard/layout'); ?>
         <section>
             <?php echo $A_vue['body'] ?>
--- Vues/standard/pied.php
+++ Vues/standard/pied.php
@@ -1,3 +1,3 @@
 <footer>
-        <p class="text-center small ">&copy; Copyright 2021 Hugo FAMECHON  & Rama HERBIN & Anthony NAVARRO  - Tous droits réservés</p>
+        <p class="m-5 text-center small">&copy; Copyright 2021 Hugo FAMECHON  & Rama HERBIN & Anthony NAVARRO  - Tous droits réservés</p>
 </footer>
